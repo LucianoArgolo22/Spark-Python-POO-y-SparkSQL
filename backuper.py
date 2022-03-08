7e0e5bd542cb4d58427cefdee072bea027a7628f
Luciano Agustín Argolo <75091406+LucianoArgolo22@users.noreply.github.com>
2022-03-08 18:33:29 -0300

    Update backuper.py

--- backuper.py
+++ backuper.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
 	Load = int(os.path.expandvars("$Load"))
 	History_load = str(os.path.expandvars("$History_load"))
 	History_load = True if History_load == "True" or History_load == True else False
-
+	data_base = str(os.path.expandvars("$data_base"))
+	
 	session_y_logs = SparkSessionYLogs()
 	spark = session_y_logs.get_contexts()
 	log = session_y_logs.loginfo()
@@ -29,16 +30,16 @@ if __name__ == "__main__":
 
 		if History_load:
 			tabla = str(tabla)
-			tabla_History_load = ArmadoDeQueriesCargaInicial(tabla, spark)	
+			tabla_History_load = ArmadoDeQueriesCargaInicial(tabla, spark, data_base)	
 			tabla_History_load.sql_sql(Load)
 
 		else:
 			tabla = str(tabla)
-			tabla_bkup = ArmadoDeQueries(tabla, spark)
+			tabla_bkup = ArmadoDeQueries(tabla, spark, data_base)
 			tabla_bkup.sql_sql()
-			delete_partitions = BorradoParticiones(tabla, spark)
+			delete_partitions = BorradoParticiones(tabla, spark, data_base)
 			bash_generator.append_strings(string_to_write=delete_partitions.partition_location(Max_over_delete))
 			delete_partitions.drop_partitions(Max_over_delete)
 
 	bash_generator.read_write_file()
-	bash_generator.bash_command("hdfs dfs -put -f ComandosBash.sh /santander/bi-corp/bdr/bkup/bkup_files")
+	bash_generator.bash_command("hdfs dfs -put -f ComandosBash.sh path/to/bkup_files")
