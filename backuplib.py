8ebdc324a8d06dd84fd10d5c56bebf962fb9f395
Luciano Agustín Argolo <75091406+LucianoArgolo22@users.noreply.github.com>
2022-05-05 18:10:02 -0300

    Update backuplib.py

--- backuplib.py
+++ backuplib.py
@@ -20,25 +20,55 @@ class SparkSessionYLogs:
 		Creates a spark session.
 		:return: A spark session.
 		'''
+		print("""────────────────────██████──────────
+──────────────────██▓▓▓▓▓▓██────────
+────────────────██▓▓▓▓▒▒▒▒██────────
+────────────────██▓▓▒▒▒▒▒▒██────────
+──────────────██▓▓▓▓▒▒▒▒██──────────
+──────────────██▓▓▒▒▒▒▒▒██──────────
+────────────██▓▓▓▓▒▒▒▒▒▒██──────────
+────────────████▒▒████▒▒██──────────
+────────────██▓▓▒▒▒▒▒▒▒▒██──────────
+──────────██────▒▒────▒▒██──────────
+──────────████──▒▒██──▒▒██──────────
+──────────██────▒▒────▒▒██──────────
+──────────██▒▒▒▒▒▒▒▒▒▒▒▒██──────────
+──────────████████████▒▒▒▒██────────
+────────██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██──────
+──────██▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒██────
+────██▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒██──
+──██▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒██
+██▓▓▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒██
+██▓▓▒▒▓▓▒▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒▒██
+██▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓██
+──████▐▌▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐▌▐▌████──
+────██▐▌▐▌▌▌▌▌▌▌▌▌▐▌▐▌▐▌▐▌▌▌▐▌██────
+────██▌▌▐▌▐▌▌▌▐▌▌▌▌▌▐▌▌▌▌▌▌▌▌▌██────
+──────██▌▌▐▌▐▌▐▌▐▌▐▌▐▌▐▌▌▌▌▌██──────
+──────██▐▌▐▌▐▌████████▐▌▌▌▌▌██──────
+────────██▒▒██────────██▒▒██────────
+────────██████────────██████────────""")
 		spark = SparkSession.builder \
 	        .config("spark.hadoop.hive.exec.dynamic.partition", "true") \
 	        .config("spark.hadoop.hive.exec.dynamic.partition.mode", "nonstrict") \
+			.config("spark.dynamicAllocation.enabled", "true") \
 			.enableHiveSupport() \
 			.getOrCreate()
 		self.spark = spark
 		return spark
 
 class CargaAbstracta:
-	def __init__(self, tabla, spark, data_base, partition = None):
+	def __init__(self, tabla, spark, data_base, partition = None, limit_show = 10000):
 		'''
 		Constructor for the class.
 		:param tabla: The dataset.
 		:param partition: The partition of the dataset.
 		'''
 		self.partition = partition
-		self.tabla = tabla
+		self.tabla = str(tabla)
 		self.spark = spark
 		self.data_base = data_base
+		self.limit_show = limit_show
 
 
 	def column_names(self):
@@ -49,6 +79,14 @@ class CargaAbstracta:
 		query = self.spark.sql(f"select * from {self.data_base}.{self.tabla} limit 0")
 		return query.columns
 
+	def column_types(self):
+		'''
+		Returns the column types of the table.
+		:return: A list of column types.
+		'''
+		columns_description = self.spark.sql("""describe X""").collect()
+		return  [columns_description[i][1] for i in range(len(columns_description))]
+
 	def colum_names_without_string(self):
 		'''
 		Returns the column names of the dataset.
@@ -56,19 +94,44 @@ class CargaAbstracta:
 		'''
 		return ", ".join(self.column_names())
 
-	def column_names_string(self):
+	#------------------------
+
+	def column_types(self):
+		'''
+		Returns the column types of the table.
+		:return: A list of column types.
+		'''
+		columns_description = self.spark.sql(f"""describe {self.data_base}.{self.tabla}""").collect()
+		return  [columns_description[i][1] for i in range(len(columns_description))]
+
+	def column_types_filtered_partition(self, partitioned = True):
 		'''
 		Returns a string of the column names separated by a comma.
 		:return: A string of the column names separated by a comma.
 		'''
-		return ",".join([column + " string" for column in self.filtering_partition_from_column_names()])
+		partition_columns = self.get_partition_names() if partitioned else " "
+		columns_description = self.spark.sql(f"""describe {self.data_base}.{self.tabla}""").collect()
+		return  [columns_description[i][1] for i in range(len(columns_description)) if columns_description[i][0] not in partition_columns]
 
-	def filtering_partition_from_column_names(self):
-		"""
-		Filtra las particiones de los nombres de las columnas
-		"""
-		partition_columns = self.get_partition_names()
+	def column_names_string(self, partitioned=True):
+		'''
+		Returns a string of the column names separated by a comma.
+		:return: A string of the column names separated by a comma.
+		'''
+		columns, types = self.filtering_partition_from_column_names(partitioned=partitioned), self.column_types_filtered_partition(partitioned=partitioned)
+		return ", ".join([columns[i] +"  "+  types[i] for i in range(len(columns))])
+    #---------------------
+
+	def filtering_partition_from_column_names(self, partitioned = True):
+		'''
+		Filters the partitions from de column names
+		:return: a list of column names that without partition columns
+		'''
+		
+		partition_columns = self.get_partition_names() if partitioned else [" "]
 		return [column for column in self.column_names() if column not in partition_columns]
+
+
 
 	def insert_partition(self):
 		'''
@@ -106,12 +169,15 @@ class CargaAbstracta:
 		tablas = self.spark.sql(f"show tables in {self.data_base}").collect()
 		return [tabla[1] for tabla in tablas]
 
-	def tabla_in_DB(self):
+	def tabla_in_DB(self, bkup = False):
 		'''
 		Checks if the table is present in the database.
 		:return: True if the table is present in the database, False otherwise.
 		'''
-		return self.tabla in self.tablas_DB()
+		backup = self.tabla + "_bkup_dag" if bkup else self.tabla 
+		log.info(f"tabla_in_DB -- Self.tabla + backup : { backup}")
+		log.info(f"tabla_in_DB -- { backup in self.tablas_DB()}")
+		return backup in self.tablas_DB()
 
 
 	def get_partition_names(self):
@@ -182,8 +248,8 @@ class CargaAbstracta:
 		EX: ["partition_date=2021-01-12", "tabla=prueba"]
 		notice that within each element there are no quotes for each value, but for the entire element
 		'''
-		log.info(f"Partición que recibe get_partition(): {partition}")
 		tabla_partition = self.tabla + "_bkup_dag" if bkup else self.tabla	
+		log.info(f"Partición que recibe get_partition(): {self.partition_table(tabla_partition).collect()[partition][0]}")
 		return self.partition_table(tabla_partition).collect()[partition][0].split("/")
 
 	def partition_table(self, tabla_partition):
@@ -250,7 +316,7 @@ class ArmadoDeQueries(CargaAbstracta):
 						({self.column_names_string()}) 
 						partitioned by ({partitions})
 						 stored as parquet 
-						 location 'path/to/bkup_dag/{self.tabla}_bkup_dag'"""
+						 location 'hdfs://path/to/{self.data_base}/{self.tabla}_bkup_dag'"""
 		log.info(f"La Query generada para crear la tabla backup de '{self.tabla}' es: \n {string}")
 		return string
 
@@ -281,25 +347,29 @@ class ArmadoDeQueries(CargaAbstracta):
 		'''
 			
 		if self.tabla_in_DB():
-			log.info(f"La Tabla '{self.tabla}' se encuentra en la base de datos {self.data_base}")f
-			self.spark.sql(self.create_table_query())
+			log.info(f"La Tabla '{self.tabla}' se encuentra en la base de datos {self.data_base}")
 			
-			if self.validacion_tiene_particiones():
-				log.info(f"La Tabla '{self.tabla}'_bkup_dag tiene particiones previas")
-				
-				if self.validacion_volumen_registros_particiones():
-					log.info(f"La última partición de la tabla '{self.tabla}'_bkup_dag no posee el mismo volumen de datos que la última partición de la '{self.tabla}', se asume son distintas, se procede a cargarla")
-					self.spark.sql(self.insert_into_query())
-					log.info(f"TABLE {self.tabla}_bkup_dag LOADED")
+			if self.validacion_tiene_particiones(bkup=False):
+				self.spark.sql(self.create_table_query())
+				log.info(f"La Tabla '{self.tabla}' tiene particiones")
+				if self.validacion_tiene_particiones():
+					log.info(f"La Tabla '{self.tabla}'_bkup_dag tiene particiones previas")
+					
+					if self.validacion_volumen_registros_particiones():
+						log.info(f"La última partición de la tabla '{self.tabla}'_bkup_dag no posee el mismo volumen de datos que la última partición de la '{self.tabla}', se asume son distintas, se procede a cargarla")
+						self.spark.sql(self.insert_into_query())
+						log.info(f"TABLE {self.tabla}_bkup_dag LOADED")
+					
+					else:
+						log.info(f"La última partición de la tabla '{self.tabla}'_bkup_dag posee el mismo volumen de datos que la última partición de la '{self.tabla}', se asume son iguales, no se carga")
+						log.info(f"TABLE {self.tabla}_bkup_dag NOT LOADED, TIENEN MISMO VOLUMEN AMBAS PARTICIONES DE AMBAS TABLAS")
 				
 				else:
-					log.info(f"La última partición de la tabla '{self.tabla}'_bkup_dag posee el mismo volumen de datos que la última partición de la '{self.tabla}', se asume son iguales, no se carga")
-					log.info(f"TABLE {self.tabla}_bkup_dag NOT LOADED, TIENEN MISMO VOLUMEN AMBAS PARTICIONES DE AMBAS TABLAS")
-			
+					log.info(f"La Tabla {self.tabla}_bkup_dag no tiene particiones previas")
+					self.spark.sql(self.insert_into_query())
+					log.info(f"TABLE {self.tabla}_bkup_dag LOADED")
 			else:
-				log.info(f"La Tabla {self.tabla}_bkup_dag no tiene particiones previas")
-				self.spark.sql(self.insert_into_query())
-				log.info(f"TABLE {self.tabla}_bkup_dag LOADED")
+				log.info(f"La Tabla {self.tabla} no tiene particiones")
 
 class ArmadoDeQueriesCargaInicial(ArmadoDeQueries):
 
@@ -319,7 +389,68 @@ class ArmadoDeQueriesCargaInicial(ArmadoDeQueries):
 		log.info(f"La Query generada para insertar datos en la tabla backup de '{self.tabla}_bkup_dag' es: \n {string}")
 		return string
 
+
 	def sql_sql(self,partition):
+		'''
+		This function checks if the table is already in the database. If it is, it creates a backup table.
+		:param partition: The partition of the table to be loaded.
+		:return: None
+		'''
+		if self.tabla_in_DB() :
+			if self.validacion_tiene_particiones(bkup=False):
+				log.info(f"La Tabla '{self.tabla}' se encuentra en la base de datos {self.data_base}")
+				self.spark.sql(self.create_table_query())
+				self.spark.sql(self.insert_into_query(partition))
+				log.info(f"TABLE {self.tabla}_bkup_dag LOADED")
+			else:
+				log.info(f"La Tabla '{self.tabla}' NO tiene datos para cargar")
+		else:
+			log.info(f"La Tabla '{self.tabla}' NO se encuentra en la base de datos {self.data_base}")
+
+
+
+class ArmadoDeQueriesNoPartition(ArmadoDeQueriesCargaInicial):	
+	def validacion_mismo_volumen_particion_previa(self):
+		'''
+		This function checks if the volume of the partition is the same as the previous partition.
+		:return: A query that returns a boolean value.
+		'''
+		return f"""select 
+						CASE 
+								when campo1 = campo2 then False 
+								else True
+								end as cumple
+						from    (select 1 as id, count(*) as campo1 from {self.data_base}.{self.tabla} ) a 
+								inner join (select 1 as id, count(*) as campo2 from {self.data_base}.{self.tabla}_bkup_dag) b
+								on  a.id = b.id	"""
+
+
+	def create_table_query(self):
+		'''
+		Creates a query to create a backup table for the given table.
+		:return: A string containing the query.
+		'''
+		string = f"""	                 create external table if not exists {self.data_base}.{self.tabla}_bkup_dag 
+						({self.column_names_string(partitioned=False)}) 
+							stored as parquet 
+							location 'hdfs://path/to/{self.data_base}/{self.tabla}_bkup_dag'"""
+		log.info(f"La Query generada para crear la tabla backup de '{self.tabla}' es: \n {string}")
+		return string
+
+	def insert_into_query(self):
+		'''
+		Generates a query to insert data into the backup table.
+		:param partition: The partition that is being inserted into the table.
+		:return: A string containing the query to be executed.
+		'''
+		string = f"""	                insert overwrite table {self.data_base}.{self.tabla}_bkup_dag 
+						select *
+						from {self.data_base}.{self.tabla}
+						"""
+		log.info(f"La Query generada para insertar datos en la tabla backup de '{self.tabla}_bkup_dag' es: \n {string}")
+		return string
+
+	def sql_sql(self):
 		'''
 		This function checks if the table is already in the database. If it is, it creates a backup table.
 		:param partition: The partition of the table to be loaded.
@@ -327,9 +458,113 @@ class ArmadoDeQueriesCargaInicial(ArmadoDeQueries):
 		'''
 		if self.tabla_in_DB():
 			log.info(f"La Tabla '{self.tabla}' se encuentra en la base de datos {self.data_base}")
-			self.spark.sql(self.create_table_query())
-			self.spark.sql(self.insert_into_query(partition))
-			log.info(f"TABLE {self.tabla}_bkup_dag LOADED")
+			if self.tabla_in_DB(bkup=True):
+				log.info(f"La Tabla '{self.tabla}_bkup_dag' se encuentra en la base de datos {self.data_base}")
+				if self.validacion_volumen_registros_particiones():
+					log.info(f"La Tabla '{self.tabla}' tiene particiones")
+					self.spark.sql(self.insert_into_query())
+					log.info(f"TABLE {self.tabla}_bkup_dag LOADED")
+				else:
+					log.info(f"La Tabla '{self.tabla}' Tiene el mismo volumen, se asumen datos iguales")
+			else:
+				log.info(f"La Tabla '{self.tabla}_bkup_dag' NO se encuentra en la base de datos {self.data_base}")
+				self.spark.sql(self.create_table_query())
+				self.spark.sql(self.insert_into_query())
+				log.info(f"TABLE {self.tabla}_bkup_dag LOADED")
+		else:
+			log.info(f"La Tabla '{self.tabla}' NO se encuentra en la base de datos {self.data_base}")
+
+
+class Validador(CargaAbstracta):
+	def conteo_(self, working_day):
+		if self.tabla_in_DB():
+			if self.validacion_esta_particionada():
+				log.info(f"Particiones en {self.tabla}: {[partition[0].split('=')[1] for partition in self.spark.sql(f'show partitions {self.data_base}.{self.tabla}').collect()[::-1]]}")
+				return self.spark.sql(f"select count(*) from {self.data_base}.{self.tabla} where partition_date='{working_day}'").collect()[0][0]
+			else:
+				log.info(f"Tabla {self.tabla} sin particionar")
+				return self.spark.sql(f"select count(*) from {self.data_base}.{self.tabla} ").collect()[0][0]
+		else:
+			print(f"La tabla {self.tabla} no se encuentra en la base de datos {self.data_base}")
+			return 0
+	
+	def validacion_esta_particionada(self):
+		try:
+			self.spark.sql(f"show partitions {self.data_base}.{self.tabla}")
+			has_partitions = True
+		except:
+			has_partitions = False
+		return has_partitions
+			
+	def creador_join(self, campo, i):
+		if i == 0:
+				return f"on coalesce(aux2.{campo}, 'null') = coalesce(aux1.{campo}, 'null')"
+		return f"and coalesce(aux2.{campo}, 'null') = coalesce(aux1.{campo}, 'null')"
+
+	def generador_string(self, n):
+		last_string  = "  ".join([self.creador_join(campo, i) for i, campo in enumerate(self.column_names())]) 
+		return last_string + f" where aux{n}.{self.column_names()[0]} is NULL" 
+		 
+	def where_partition(self): 
+		if self.validacion_esta_particionada():
+			return f"where partition_date = '{self.partition}'"
+		else:
+			return " "
+
+	def with_tables(self):
+		string=f"""with tabla_aux as (
+			select * from {self.data_base}.{self.tabla}
+			{self.where_partition()}
+			),
+			tabla_aux_bkup_dag as (
+			select * from {self.data_base}.{self.tabla}_bkup_dag
+			{self.where_partition()}
+			)"""
+		return string	
+
+	def query_atributos(self, n, tabla = "TABLA PROD", with_tables_optional = True):
+		if with_tables_optional == True:
+			with_tables = self.with_tables()
+		else:
+			with_tables = " "
+		return f"""{with_tables}
+		select '{tabla}' as tabla, aux{n}.* from tabla_aux_bkup_dag aux2
+		FULL JOIN tabla_aux aux1 """
+
+	def final_query_to_execute(self, tablas_a_traer):
+		if tablas_a_traer=='prod':
+			query = self.query_atributos(n=1, tabla= "TABLA PROD", with_tables_optional = True).replace('\n','') + self.generador_string(n = 2)
+		elif tablas_a_traer=='bkup':
+			query = self.query_atributos(n=2, tabla= "TABLA BKUP", with_tables_optional = True).replace('\n','') + self.generador_string(n = 1)
+		log.info(f"La query que ejecuta la validación de atributos es: {query} ")
+		return query
+
+	def sql_sql(self, tablas_a_traer): 
+		return self.spark.sql(self.final_query_to_execute(tablas_a_traer))
+
+	def union_all_string(self, campo, i):
+		return f"""(select 1 as id, 'TABLA PROD' as tabla, count(distinct({campo})) as {campo} from  tabla_aux
+			union all
+			select 2 as id, 'TABLA BKUP' as tabla, count(distinct({campo})) as {campo} from  tabla_aux_bkup_dag ) a{i}"""
+
+	def creador_join_distintos(self ,campos, campo, i):
+		if i == 0:
+			return f"""select  a0.tabla, {", ".join(self.column_names())} from 
+			{self.union_all_string(campo, i)}"""
+		return f"""inner join
+			{self.union_all_string(campo, i)}
+			on a{i}.id = a{int(i)-1}.id """
+
+	def unir_queries(self):
+		query = ""
+		for i, campo in enumerate(self.column_names()):
+			query += " " + self.creador_join_distintos(self.column_names(), campo, i )
+		return  self.with_tables() + query
+
+
+	def valores_distintos_sql(self):
+			log.info(f"Resultado de unir queries {self.unir_queries()}")
+			self.spark.sql(self.unir_queries()).show(truncate = False)
 
 
 class BorradoParticiones(CargaAbstracta):
@@ -343,7 +578,10 @@ class BorradoParticiones(CargaAbstracta):
 		 Row(partition_date='2021-08')]
 		"""
 		log.info(f"filter_partitions_to_delete -- Load = {Load}")
-		return self.spark.sql(self.select_partitions_string()).collect()[Load:]
+		filtered_partitions_to_delete = self.spark.sql(self.select_partitions_string()).collect()
+		log.info(f"filter_partitions_to_delete -- filtered_partitions_to_delete = {filtered_partitions_to_delete}")
+		log.info(f"filter_partitions_to_delete -- filtered_partitions_to_delete[Load:] = {filtered_partitions_to_delete[Load:]}")
+		return filtered_partitions_to_delete[Load:]
 
 
 	def partition_location(self,Load = 3):
@@ -353,13 +591,17 @@ class BorradoParticiones(CargaAbstracta):
 		:return: A string with the commands to delete the partitions.
 		'''
 		log.info("Partition_location -- Validando si el bkup tiene particiones")
-		if self.validacion_tiene_particiones(cantidad = Load):
-			log.info("Partition_location -- True")
-			filtered_partitions = self.filter_partitions_to_delete(Load)
-			return "\n".join([f"hdfs dfs -rm -r  /path/to/bkup_dag/{self.tabla}_bkup_dag/{list(self.partitions_dict())[0]}={partition[0]}" for partition in filtered_partitions]) 
-		
+		if self.tabla_in_DB(bkup=True):
+			if self.validacion_tiene_particiones(cantidad = Load):
+				log.info("Partition_location -- True")
+				filtered_partitions = self.filter_partitions_to_delete(Load)
+				return "\n".join([f"hdfs dfs -rm -r  /path/to/{self.data_base}/{self.tabla}_bkup_dag/{list(self.partitions_dict())[0]}={partition[0]}" for partition in filtered_partitions]) 
+			
+			else:
+				log.info("Partition_location -- False")
+				return " "
 		else:
-			log.info("Partition_location -- False")
+			log.info("Partition_location, No bkup table found -- False")
 			return " "
 
 	def drop_partitions_query(self, max_partition_limit):
@@ -390,10 +632,11 @@ class BorradoParticiones(CargaAbstracta):
 		:param max_partition_limit: The maximum number of partitions to be retained.
 		:return: None
 		'''
-		query_dropeo = self.drop_partitions_query(max_partition_limit)
-		log.info(f"Drop_partitions  Query:-- {query_dropeo}")
-		for query in query_dropeo:
-			self.spark.sql(query)
+		if self.tabla_in_DB(bkup=True):
+			query_dropeo = self.drop_partitions_query(max_partition_limit)
+			log.info(f"Drop_partitions  Query:-- {query_dropeo}")
+			for query in query_dropeo:
+				self.spark.sql(query)
 
 
 class ComandosBash:
@@ -428,9 +671,7 @@ class ComandosBash:
 		out = out.decode("utf-8")
 		return out.split("\n")
 
-	# def bash_already_exists(self):
-	# 	ls_grep_ComandosBash = " , ".join(self.bash_command(strings="hdfs dfs -ls /path/to/bkup_files |grep ComandosBash.sh "))
-	# 	return "ComandosBash.sh" in  ls_grep_ComandosBash
+
 
 	def read_write_file(self):
 		'''
@@ -440,7 +681,7 @@ class ComandosBash:
 		:return: None
 		'''
 		with open(self.path + self.filename, self.mode) as f:
-			log.info(f"Read Write File -- Creando archivo ComandosBash.sh ")
+			log.info(f"Read Write File -- Creando archivo Bash")
 			strings = '\n'.join(self.string_to_write)
 			log.info(f"""Read Write File -- Las particiones a borrar son: {strings}""")
 			if self.mode == "w":
